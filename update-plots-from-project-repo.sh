1f54e9a0fc676613c2389c25c761f81e87cc179d
Jamie R. Oaks <joaks1@gmail.com>
2024-10-03 16:56:28 -0500

    Adding script to get plots from project repo.

---
+++ update-plots-from-project-repo.sh
@@ -0,0 +1,119 @@
+#! /bin/bash
+
+set -e
+
+write_hline() {
+    echo "########################################################################"
+}
+
+write_banner_msg() {
+    echo ""
+    write_hline
+    echo "$@"
+    write_hline
+}
+
+# Display help info
+usage () {
+echo ""
+echo "Usage:"
+echo "  $0 [ OPTIONS ] [ GIT-REF ]"
+echo ""
+echo "Example:"
+echo ""
+echo "  $0 HEAD"
+echo ""
+echo "Optional flag arguments ( 'OPTIONS' ):"
+echo "  -h|--help         Show help message and exit."
+echo ""
+echo "Positional arguments ( 'GIT-REF' ):"
+echo "  The Git ref for the codiv backoff project repo. Plots from this commit"
+echo "  will be copied into this manuscript repo."
+echo "  If no ref is provided, the default is to use the most recent commit of"
+echo "  of the project repo."
+echo ""
+}
+
+this_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Remove the temporary directory created for project repo
+clean_up() {
+  test -d "$tmp_dir" && rm -rf "${tmp_dir}/.git" && rm -r "$tmp_dir"
+}
+# Ensure temp dir gets deleted on exit
+trap clean_up EXIT
+# Make temp dir for cloning project repo 
+# The or ("||") syntax is to support different flavors of `mktemp`; Darwin's
+# mktemp behaves differently than Gnu's.
+tmp_dir="$(mktemp -d 2>/dev/null || mktemp -d -t 'tmp_codiv_bakeoff')"
+
+project_dir="$tmp_dir"
+
+project_plot_dir="${project_dir}/results/plots"
+project_emp_plot_dir="${project_dir}/results/empirical-plots"
+image_dir="${this_dir}/images"
+if [ ! -d "$image_dir" ]
+then
+    mkdir "$image_dir"
+fi
+dest_dir="${image_dir}/from-project-repo"
+if [ ! -d "$dest_dir" ]
+then
+    mkdir "$dest_dir"
+fi
+
+
+###############################################################################
+# Process command line args
+
+positional_args=()
+
+while [ -n "$1" ]
+do
+    case $1 in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -* | --*)
+            echo "Unrecognized option flag: \"$1\""
+            usage
+            exit 1
+            ;;
+        *)
+            positional_args+=("$1")
+            shift
+            ;;
+    esac
+done
+
+###############################################################################
+# Sanity checks
+
+if [ "${#positional_args[@]}" -ge "2" ]
+then
+    write_banner_msg "ERROR: You provided ${#positional_args[@]} positional arguments; only 1 is allowed"
+    exit 1
+fi
+
+write_banner_msg "Cloning codiv bakeoff project repo..."
+git clone git@github.com:phyletica/codiv-sanger-bake-off.git "$project_dir"
+
+git_ref="$(cd "$project_dir"; git rev-parse --short HEAD)"
+if [ "${#positional_args[@]}" = "1" ]
+then
+    git_ref="${positional_args[0]}"
+fi
+
+commit_sha="$(cd "$project_dir"; git rev-parse --short "$git_ref")"
+
+# Make sure we are on the correct commit so we copy the correct images
+write_banner_msg "Checking out commit $commit_sha  of bakeoff project repo..."
+(
+    cd "$project_dir"
+    git checkout -b tmp "$commit_sha"
+)
+
+write_banner_msg "Copying plot directories from project repo..."
+cp -r "$project_plot_dir" "$dest_dir"
+cp -r "$project_emp_plot_dir" "$dest_dir"
